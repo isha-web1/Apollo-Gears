e93e09736e261a71d51cf96e66c37ddd8ff515b1
isha-web1 <isha.web1@gmail.com>
2025-09-02 12:41:55 +0600

    authentication system added

---
+++ src/app/(commonLayout)/components/page/shared/googleLoginBtn.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button } from "@heroui/react";
+import { signIn } from "next-auth/react";
+
+const GoogleLoginBtn = () => {
+  return (
+    <Button
+      onClick={() => {
+        signIn("google", { callbackUrl: "/" });
+      }}
+    >
+      Log In With Google
+    </Button>
+  );
+};
+
+export default GoogleLoginBtn;
